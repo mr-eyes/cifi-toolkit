9d7d61344cfaaa0dc33f8c656de54b297388e613
Mohamed Abuelanin <mabuelanin@gmail.com>
2026-02-08 10:28:11 -0800

    bump version to 0.2.3

--- src/cifi/__init__.py
+++ src/cifi/__init__.py
@@ -4,7 +4,7 @@ cifi - toolkit for downstream processing of CiFi long reads.
 https://dennislab.org/cifi
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from ._core import (
     FilterResult,
